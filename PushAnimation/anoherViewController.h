f9a383de75988e37ebbb16bf6b1687e730b62be9
Fabian Rosales <fabian7593@gmail.com>
2015-10-26 08:56:37 -0600

    Update anoherViewController.h

--- PushAnimation/anoherViewController.h
+++ PushAnimation/anoherViewController.h
@@ -2,7 +2,7 @@
 //  anoherViewController.h
 //  PushAnimation
 //
-//  Created by Cesar Gomez on 10/9/15.
+//  Created by Fabian Rosales on 10/9/15.
 //  Copyright (c) 2015 KeyBellSoftCR. All rights reserved.
 //
 
